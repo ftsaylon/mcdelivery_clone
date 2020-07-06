cebe32520a60143b30d2e85270eca2cbb6ac7f47
Francis Saylon <28182748+ftsaylon@users.noreply.github.com>
2020-07-07 00:05:28 +0800

    Added app icon

--- lib/screens/order_tracker_screen.dart
+++ lib/screens/order_tracker_screen.dart
@@ -37,42 +37,45 @@ class OrderTrackerScreen extends StatelessWidget {
       ),
       body: Center(
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Track your McDo Order',
-                style: TextStyle(fontSize: 18),
-              ),
-              Text(
-                'from our store to your door',
-                style: TextStyle(
-                  color: Colors.grey,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Track your McDo Order',
+                  style: TextStyle(fontSize: 18),
                 ),
-              ),
-              SizedBox(height: 12),
-              Container(
-                child: Image.asset('assets/images/order_tracker/step1.png'),
-              ),
-              Container(
-                child: (order.isProcessed)
-                    ? Image.asset('assets/images/order_tracker/step2.png')
-                    : Image.asset(
-                        'assets/images/order_tracker/step2-disabled.png'),
-              ),
-              Container(
-                child: (order.isBeingPrepared)
-                    ? Image.asset('assets/images/order_tracker/step3.png')
-                    : Image.asset(
-                        'assets/images/order_tracker/step3-disabled.png'),
-              ),
-              Container(
-                child: (order.isOnTheWay)
-                    ? Image.asset('assets/images/order_tracker/step4.png')
-                    : Image.asset(
-                        'assets/images/order_tracker/step4-disabled.png'),
-              ),
-            ],
+                Text(
+                  'from our store to your door',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(height: 12),
+                Container(
+                  child: Image.asset('assets/images/order_tracker/step1.png'),
+                ),
+                Container(
+                  child: (order.isProcessed)
+                      ? Image.asset('assets/images/order_tracker/step2.png')
+                      : Image.asset(
+                          'assets/images/order_tracker/step2-disabled.png'),
+                ),
+                Container(
+                  child: (order.isBeingPrepared)
+                      ? Image.asset('assets/images/order_tracker/step3.png')
+                      : Image.asset(
+                          'assets/images/order_tracker/step3-disabled.png'),
+                ),
+                Container(
+                  child: (order.isOnTheWay)
+                      ? Image.asset('assets/images/order_tracker/step4.png')
+                      : Image.asset(
+                          'assets/images/order_tracker/step4-disabled.png'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
